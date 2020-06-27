39bf31956552cf3264b7536ded0b56e1cbd26507
Sergei Makhaev <qwaqalkaib@gmail.com>
2020-06-27 11:54:55 +0500

    Add all files

---
+++ app/src/main/java/com/qegle/motionlayout/BackdropActivity.kt
@@ -0,0 +1,12 @@
+package com.qegle.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BackdropActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backdrop_start)
+    }
+}
